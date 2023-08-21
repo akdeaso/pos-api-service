54a1ba3ba74273f96d2c29b0e9731df63d651d7c
AKDEAS\akdea <akdeaso@gmail.com>
2023-08-21 19:43:59 +0700

    test deploy

--- database/index.js
+++ database/index.js
@@ -1,15 +1,21 @@
 const mongoose = require("mongoose");
 const { dbHost, dbPass, dbPort, dbUser, dbName } = require("../app/config");
 
-mongoose.connect(
-  `mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin`,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-  }
-);
+//for local
+// mongoose.connect(
+//   `mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin`,
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: false,
+//     useCreateIndex: true,
+//   }
+// );
+// const db = mongoose.connection;
+
+mongoose.connect(process.env.MONGO_URI);
 const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", () => console.log(`Server database terhubung ${db.host}`));
 
 module.exports = db;
